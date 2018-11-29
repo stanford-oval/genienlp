827e27d472be9c94bc75e7319a7130c0bd6df94d
mehrad <mehrad@stanford.edu>
2018-11-28 18:00:05 -0800

    updates on post_processing

--- utils/post_process_decoded_results.py
+++ utils/post_process_decoded_results.py
@@ -31,7 +31,8 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('--input_sentences', default='./eval.en-tt.en', type=str)
+parser.add_argument('--reference_gold', default='./test/test.en-tt.tt', type=str)
+parser.add_argument('--input_sentences', default='./test/test.en-tt.en', type=str)
 parser.add_argument('--gold_program', default='./test/almond.gold.txt', type=str)
 parser.add_argument('--predicted_program', default='./test/almond.txt', type=str)
 parser.add_argument('--output_file', default='./test/out_file', type=str)
@@ -83,12 +84,50 @@ def get_quotes(pred, gold):
 
     return quotes_list_pred, quotes_list_gold
 
-with open(args.input_sentences, 'r') as input_file,\
-        open(args.gold_program, 'r') as gold_file,\
-        open(args.predicted_program, 'r') as pred_file,\
-        open(args.output_file, 'w') as out:
+def find_indices(ref, shuf):
 
-    for line in zip(input_file, gold_file, pred_file):
+    ref_list = []
+    shuf_list = []
+
+    with open(ref, 'r') as f_ref:
+        for line in f_ref:
+            line = line[:-1].lower()
+            ref_list.append(line)
+
+    with open(shuf, 'r') as f_shuf:
+        for line in f_shuf:
+            line = line[1:-2].replace(r'\"', '"').lower()
+            shuf_list.append(line)
+
+    # ref_list_enum = list(enumerate(ref_list))
+    # shuf_list_enum = list(enumerate(shuf_list))
+
+    indices = []
+    for i, val in enumerate(shuf_list):
+        common1 = list(set([c for c in val if c not in ref_list[121]]))
+        common2 = list(set([c for c in ref_list[121] if c not in val]))
+        indices.append(ref_list.index(val))
+
+    return indices
+
+indices = find_indices(args.reference_gold, args.gold_program)
+
+inputs = []
+with open(args.input_sentences, 'r') as input_file:
+    for line in input_file:
+        inputs.append(line)
+
+    res = [inputs[i] for i in indices]
+
+
+
+
+
+with open(args.gold_program, 'r') as gold_file,\
+     open(args.predicted_program, 'r') as pred_file,\
+     open(args.output_file, 'w') as out:
+
+    for line in zip(res, gold_file, pred_file):
         input, gold, pred = line
         input = input.replace(r'<s>', '')
         input = input.strip()
@@ -108,4 +147,6 @@ with open(args.input_sentences, 'r') as input_file,\
             out.write(' || ' + str("{0:.2f}".format(correct_quotes)) + '%_correct_quotes')
         if correct_tokens != False:
             out.write(' || ' + str("{0:.2f}".format(correct_tokens)) + '%_correct_tokens')
+        out.write('\n')
+        out.write('\n')
         out.write('\n')
