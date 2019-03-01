7738b66e72348e7bd8150d9b32d68bdaf3a65acc
mehrad <mehrad@stanford.edu>
2019-02-28 19:52:13 -0800

    updates

--- utils/post_process_decoded_results.py
+++ utils/post_process_decoded_results.py
@@ -172,8 +172,12 @@ def run():
                 input, gold, pred, id = line
                 id = id.strip()
                 input = input.replace(r'<s>', '').strip()
-                gold = gold.strip().replace(r'\"', '"').replace(r'\/', '/').strip('"')
+                gold = gold.strip().replace(r'\"', '"').replace(r'\/', '/')
+                if gold[0] == gold[-1] == '"':
+                    gold = gold[1:-1]
                 pred = pred.strip().replace(r'\"', '"').replace(r'\/', '/')
+                if pred[0] == pred[-1] == '"':
+                    pred = pred[1:-1]
 
                 out_raw.write(id + '\t' + input + '\t' + gold + '\t' + pred)
                 out_raw.write('\n')
