a44bde66936b06a4a5311ed33b62c273161c46c7
Giovanni Campagna <gcampagn@cs.stanford.edu>
2019-03-01 15:21:48 -0800

    Don't import unused tensorflow

--- models/common.py
+++ models/common.py
@@ -7,7 +7,6 @@ import os
 import sys
 import numpy as np
 import torch.nn as nn
-import tensorflow
 
 from torch.nn.utils.rnn import pad_packed_sequence as unpack
 from torch.nn.utils.rnn import pack_padded_sequence as pack
