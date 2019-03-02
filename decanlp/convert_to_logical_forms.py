4bddbada459dd7144448d014a52dc14690623f1a
Giovanni Campagna <gcampagn@cs.stanford.edu>
2019-03-01 16:08:52 -0800

    Fix typo

--- decanlp/convert_to_logical_forms.py
+++ decanlp/convert_to_logical_forms.py
@@ -27,7 +27,7 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from .util.generic_dataset import Query
+from .utils.generic_dataset import Query
 from argparse import ArgumentParser
 import os
 import re
