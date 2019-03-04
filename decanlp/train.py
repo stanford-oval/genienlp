c9d8e920a987b9e473684f0d52978a963ae6e545
Giovanni Campagna <gcampagn@cs.stanford.edu>
2019-03-03 22:50:30 -0800

    Fix logger.debug call
    It does not take more than one argument

--- decanlp/train.py
+++ decanlp/train.py
@@ -108,7 +108,7 @@ def prepare_data(args, field, logger):
 
     for task, s in zip(args.train_tasks, train_sets):
         for ex in s.examples[:10]:
-            logger.debug('examples***:', [token.strip() for token in ex.context])
+            logger.debug(f'examples***: {[token.strip() for token in ex.context]}')
 
     if args.load is None:
         logger.info(f'Getting pretrained word vectors')
