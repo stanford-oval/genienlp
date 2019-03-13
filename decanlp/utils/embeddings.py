a7ce796d765587fa395ca6d7d9592dcd072f7469
Giovanni Campagna <gcampagn@cs.stanford.edu>
2019-03-13 15:48:46 -0700

    Add Almond type embeddings
    A better representation for typed (delexicalized) constants in
    the Almond task

--- decanlp/utils/embeddings.py
+++ decanlp/utils/embeddings.py
@@ -28,10 +28,41 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+import torch
+
 import logging
 from ..text import torchtext
 
 _logger = logging.getLogger(__name__)
+
+ENTITIES = ['DATE', 'DURATION', 'EMAIL_ADDRESS', 'HASHTAG',
+            'LOCATION', 'NUMBER', 'PHONE_NUMBER', 'QUOTED_STRING',
+            'TIME', 'URL', 'USERNAME', 'PATH_NAME', 'CURRENCY']
+MAX_ARG_VALUES = 5
+
+class AlmondEmbeddings(torchtext.vocab.Vectors):
+
+    def cache(self, name, cache, url=None):
+        del name
+        del cache
+
+        dim = len(ENTITIES) + MAX_ARG_VALUES
+
+        itos = []
+        vectors = []
+        for i, entity in enumerate(ENTITIES):
+            for j in range(MAX_ARG_VALUES):
+                itos.append(entity + '_' + str(j))
+                vec = torch.zeros((dim,), dtype=torch.float32)
+                vec[i] = 1.0
+                vec[len(ENTITIES) + j] = 1.0
+                vectors.append(vec)
+
+        self.itos = itos
+        self.stoi = {word: i for i, word in enumerate(itos)}
+        self.vectors = torch.Tensor(vectors).view(-1, dim)
+        self.dim = dim
+
 
 def load_embeddings(args, logger=_logger):
     logger.info(f'Getting pretrained word vectors')
@@ -40,4 +71,7 @@ def load_embeddings(args, logger=_logger):
         glove_vectors = torchtext.vocab.GloVe(cache=args.embeddings, name="6B", dim=50)
     else:
         glove_vectors = torchtext.vocab.GloVe(cache=args.embeddings)
-    return [char_vectors, glove_vectors]
+    vectors = [char_vectors, glove_vectors]
+    if args.almond_type_embeddings:
+        vectors.append(AlmondEmbeddings())
+    return vectors
