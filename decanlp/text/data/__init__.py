aa628629bf4612ac4c9eecbe8879f12090010cc5
Giovanni Campagna <gcampagn@cs.stanford.edu>
2019-12-21 06:56:23 -0800

    Remove torchtest.Iterator and torchtext.Batch
    Now unused

--- decanlp/text/data/__init__.py
+++ decanlp/text/data/__init__.py
@@ -1,15 +1,10 @@
-from .batch import Batch
 from .dataset import Dataset
 from .field import RawField, Field, ReversibleField, SubwordField
-from .iterator import (batch, BucketIterator, Iterator,
-                       pool)
 from .pipeline import Pipeline
 from .utils import get_tokenizer, interleave_keys
 
 __all__ = ["Batch",
            "Dataset",
            "RawField", "Field", "ReversibleField", "SubwordField",
-           "batch", "BucketIterator", "Iterator",
-           "pool",
            "Pipeline",
            "get_tokenizer", "interleave_keys"]
