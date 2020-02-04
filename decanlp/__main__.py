7bc6123f0bc212be0677a804fad244216a1ee69b
Sina <s.j.semnani@gmail.com>
2020-02-04 14:55:13 -0800

    added train-paraphrase subcommand

--- decanlp/__main__.py
+++ decanlp/__main__.py
@@ -36,7 +36,8 @@ subcommands = {
     'train': ('Train a model', train.main),
     'predict': ('Evaluate a model, or compute predictions on a test dataset', predict.main),
     'server': ('Export RPC interface to predict', server.main),
-    'cache-embeddings': ('Download and cache embeddings', cache_embeddings.main)
+    'cache-embeddings': ('Download and cache embeddings', cache_embeddings.main),
+    'train-paraphrase': ('Train a paraphraser model', run_lm_finetuning.main)
 }
 
 def usage():
