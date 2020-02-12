5ffffb71311a3cb8012ffcb24cb84f720983ceb6
Sina <s.j.semnani@gmail.com>
2020-02-12 13:55:48 -0800

    added more transformation scripts

--- decanlp/transform_dataset.py
+++ decanlp/transform_dataset.py
@@ -24,7 +24,11 @@ def main():
                         help='The path to the output file.')
     parser.add_argument('--query_file', type=str,
                         help='The path to the file containing new queries.')
-    parser.add_argument('--transformation', type=str, choices=['remove_thingtalk_quotes', 'replace_queries', 'none'], default='none',
+    parser.add_argument('--thingtalk_gold_file', type=str,
+                        help='The path to the file containing the dataset with a correct thingtalk column.')
+    parser.add_argument('--num_new_queries', type=int, default=1,
+                        help='Number of new queries per old query. Valid if "--transformation replace_queries" is used.')
+    parser.add_argument('--transformation', type=str, choices=['remove_thingtalk_quotes', 'replace_queries', 'remove_wrong_thingtalk', 'none'], default='none',
                         help='The type of transformation to apply.')
     parser.add_argument('--output_columns', type=int, nargs='+', default=[0, 1, 2],
                         help='The type of transformation to apply.')
@@ -37,22 +41,40 @@ def main():
             new_queries = []
             query_file = open(args.query_file, 'r')
             for q in query_file:
-                new_queries.append(q)
-        line_count = 0
+                new_queries.append(q.strip())
+            new_query_count = 0
+        if args.transformation == 'remove_wrong_thingtalk':
+            gold_thingtalks = []
+            thingtalk_gold_file_reader = csv.reader(open(args.thingtalk_gold_file, 'r'), delimiter='\t')
+            for q in thingtalk_gold_file_reader:
+                gold_thingtalks.append(q[1].strip())
+            gold_thingtalk_count = 0
+            
         for row in tqdm(reader, desc='Lines'):
-            query = row[1]
-            thingtalk = row[2]
+            output_rows = []
             if args.transformation == 'remove_thingtalk_quotes':
-                row[2] = remove_thingtalk_quotes(thingtalk)
-            if args.transformation == 'replace_queries':
-                row[1] = new_queries[line_count]
-            output_row = ""
-            for i, column in enumerate(args.output_columns):
-                output_row += row[column]
-                if i < len(args.output_columns)-1:
-                    output_row += '\t'
-            output_file.write(output_row + '\n')
-            line_count += 1
+                row[2] = remove_thingtalk_quotes(row[2])
+            if args.transformation == 'remove_wrong_thingtalk':
+                # print(row[2])
+                if row[2] == gold_thingtalks[gold_thingtalk_count]:
+                    output_rows = [row]
+                else:
+                    output_rows = []
+                gold_thingtalk_count += 1
+            elif args.transformation == 'replace_queries':
+                for _ in range(args.num_new_queries):
+                    row[1] = new_queries[new_query_count]
+                    output_rows.append(row.copy())
+                    new_query_count += 1
+            else:
+                output_rows = [row]
+            for o in output_rows:
+                output_row = ""
+                for i, column in enumerate(args.output_columns):
+                    output_row += o[column]
+                    if i < len(args.output_columns)-1:
+                        output_row += '\t'
+                output_file.write(output_row + '\n')
 
 if __name__ == '__main__':
     main()
