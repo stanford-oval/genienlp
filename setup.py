65338cb05df0d39f99686ebd85a9c5742a7f863d
Giovanni Campagna <gcampagn@cs.stanford.edu>
2020-03-21 19:08:19 -0700

    v0.2.0a1

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='genienlp',
-    version='0.1.1',
+    version='0.2.0a1',
     
     packages=setuptools.find_packages(exclude=['tests']),
     entry_points= {
