d2eacd4e5c5347cb45907aea9e0a81a54b9fd5bf
Sina <s.j.semnani@gmail.com>
2020-05-26 23:34:22 -0700

    Add option to write examples that do not pass the filter into a file

--- genienlp/paraphrase/scripts/transform_dataset.py
+++ genienlp/paraphrase/scripts/transform_dataset.py
@@ -12,6 +12,21 @@ def is_subset(set1, set2):
     """
     return all([e in set2 for e in set1])
 
+def passes_heuristic_checks(row, args):
+    all_input_columns = ' '.join([row[c] for c in args.input_columns])
+    input_special_tokens = set(re.findall('[A-Za-z:_.]+_[0-9]', all_input_columns))
+    output_special_tokens = set(re.findall('[A-Za-z:_.]+_[0-9]', row[args.thingtalk_column]))
+    if not is_subset(output_special_tokens, input_special_tokens):
+        return False
+    _, quote_values = remove_thingtalk_quotes(row[args.thingtalk_column])
+    if quote_values is None:
+        return False # ThingTalk is not syntactic, so remove this example
+    for q in quote_values:
+        if q not in all_input_columns:
+            return False
+    return True
+    
+
 def main():
     parser = ArgumentParser()
     parser.add_argument('input', type=str,
@@ -20,6 +35,8 @@ def main():
                         help='The path to the output file.')
     parser.add_argument('--query_file', type=str,
                         help='The path to the file containing new queries.')
+    parser.add_argument('--thrown_away', type=str, default=None,
+                        help='The path to the output file that will contain inputs that were removed because of `--transformation`.')
     parser.add_argument('--thingtalk_gold_file', type=str,
                         help='The path to the file containing the dataset with a correct thingtalk column.')
     parser.add_argument('--num_new_queries', type=int, default=1,
@@ -98,8 +115,10 @@ def main():
         written_count = 0
         if args.remove_duplicates:
             seen_examples = set()
+        all_thrown_away_rows = []
         for row in tqdm(reader, desc='Lines'):
             output_rows = []
+            thrown_away_rows = []
             if args.transformation == 'remove_thingtalk_quotes':
                 row[args.thingtalk_column], _ = remove_thingtalk_quotes(row[args.thingtalk_column])
             if args.transformation == 'remove_wrong_thingtalk':
@@ -107,6 +126,7 @@ def main():
                     output_rows = [row]
                 else:
                     output_rows = []
+                    thrown_away_rows = [row]
                 gold_thingtalk_count += 1
             elif args.transformation == 'get_wrong_thingtalk':
                 if row[args.thingtalk_column] != gold_thingtalks[gold_thingtalk_count]:
@@ -141,23 +161,8 @@ def main():
             for o in output_rows:
                 output_row = ""
                 if args.remove_with_heuristics:
-                    all_input_columns = ' '.join([o[c] for c in args.input_columns])
-                    input_special_tokens = set(re.findall('[A-Za-z:_.]+_[0-9]', all_input_columns))
-                    output_special_tokens = set(re.findall('[A-Za-z:_.]+_[0-9]', o[args.thingtalk_column]))
-                    if  not is_subset(output_special_tokens, input_special_tokens) :
+                    if not passes_heuristic_checks(o, args):
                         heuristic_count += 1
-                        continue
-                    _, quote_values = remove_thingtalk_quotes(o[args.thingtalk_column])
-                    if quote_values is None:
-                        heuristic_count += 1
-                        continue
-                    should_skip = False
-                    for q in quote_values:
-                        if q not in all_input_columns:
-                            heuristic_count += 1
-                            should_skip = True
-                            break
-                    if should_skip:
                         continue
                 if args.remove_duplicates:
                     normalized_example = re.sub('\s+', '', ''.join([o[i] for i in args.no_duplication_columns]))
@@ -173,6 +178,21 @@ def main():
                     if i < len(args.output_columns)-1:
                         output_row += '\t'
                 output_file.write(output_row + '\n')
+            for o in thrown_away_rows:
+                if args.remove_with_heuristics and passes_heuristic_checks(o, args):
+                    all_thrown_away_rows.append(o)
+
+        if args.thrown_away is not None:
+            # write the thrown away exampels into a file
+            with open(args.thrown_away, 'w') as output_file:
+                for o in all_thrown_away_rows:
+                    output_row = ""
+                    for i, column in enumerate(args.output_columns):
+                        output_row += o[column]
+                        if i < len(args.output_columns)-1:
+                            output_row += '\t'
+                    output_file.write(output_row + '\n')
+
         print('Removed %d duplicate rows' % duplicate_count)
         print('Removed %d rows because the thingtalk quote did not satisfy heuristic rules' % heuristic_count)
         print('Output size is %d rows' % written_count)
