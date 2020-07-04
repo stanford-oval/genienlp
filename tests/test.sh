f8976ac6d7b70790503b9555779c707b74633227
Sina <s.j.semnani@gmail.com>
2020-07-03 17:14:24 -0700

    Tests are less resource-intensive to run on Travis

--- tests/test.sh
+++ tests/test.sh
@@ -98,10 +98,10 @@ if test ! -f $workdir/generated.tsv ; then
 fi
 
 # finetune BART for one epoch
-pipenv run python3 -m genienlp train-paraphrase --train_data_file $workdir/paraphrasing/train.tsv --eval_data_file $workdir/paraphrasing/dev.tsv --output_dir $workdir/bart-large --model_type bart --model_name_or_path bart-large --do_train --do_eval --evaluate_during_training --logging_steps 2 --save_steps 2 --max_steps 4 --save_total_limit 2 --gradient_accumulation_steps 1 --per_gpu_eval_batch_size 4 --per_gpu_train_batch_size 4 --num_train_epochs 1 --overwrite_output_dir --overwrite_cache --tensorboard_dir $workdir/save_para_text
+pipenv run python3 -m genienlp train-paraphrase --train_data_file $workdir/paraphrasing/train.tsv --eval_data_file $workdir/paraphrasing/dev.tsv --output_dir $workdir/bart-large-1 --tensorboard_dir $workdir/tensorboard/ --model_type bart --do_train --do_eval --evaluate_during_training --overwrite_output_dir --logging_steps 1000 --save_steps 1000 --max_steps 4 --save_total_limit 1 --gradient_accumulation_steps 1 --per_gpu_eval_batch_size 1 --per_gpu_train_batch_size 1 --num_train_epochs 1 --model_name_or_path bart-large
 
 # correct sentences using BART
-pipenv run python3 -m genienlp run-paraphrase --input_file $SRCDIR/dataset/almond/train.tsv --input_column 1 --output_file $workdir/generated.tsv --model_name_or_path $workdir/bart-large --length 15 --temperature 0.4 --repetition_penalty 1.0 --num_samples 4 --stop_tokens . ! ?
+pipenv run python3 -m genienlp run-paraphrase --input_file $SRCDIR/dataset/almond/train.tsv --input_column 1 --output_file $workdir/generated.tsv --model_name_or_path $workdir/bart-large-1 --length 15 --temperature 0.4 --repetition_penalty 1.0 --num_samples 4 --stop_tokens . ! ?
 
 # check if generated file exists
 if test ! $workdir/generated.tsv ; then
