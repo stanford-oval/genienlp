cbe17b097afa6651bd243c4853888d78c5f96da7
Ryan Othniel Kearns <ryankearns25@gmail.com>
2020-07-06 13:31:30 -0700

    modify seq2seq for initial ewc implementation

--- genienlp/models/general_seq2seq.py
+++ genienlp/models/general_seq2seq.py
@@ -36,6 +36,7 @@ from .mqan_encoder import MQANEncoder
 from .identity_encoder import IdentityEncoder
 from .mqan_decoder import MQANDecoder
 from .common import mask_tokens
+from torch.nn import functional as F
 
 ENCODERS = {
     'MQANEncoder': MQANEncoder,
@@ -59,6 +60,12 @@ class Seq2Seq(torch.nn.Module):
 
         if self.args.pretrain_context > 0:
             self.context_pretrain_lm_head = torch.nn.Linear(self.args.dimension, numericalizer.num_tokens)
+        
+        self.EWC_task_count = 0
+        self.ewc_gamma = 1.
+        # self.ewc = getattr(self.args, 'use_ewc', False)
+        # # hyperparam: how strong to weigh the EWC loss from the previous task
+        # self.ewc_lambda =  getattr(self.args, 'ewc_lambda', 0)
 
     def set_train_context_embeddings(self, trainable):
         self.encoder.set_train_context_embeddings(trainable)
@@ -93,9 +100,25 @@ class Seq2Seq(torch.nn.Module):
     def forward(self, batch, iteration, pretraining=False):
         # print('batch = ', batch)
         if pretraining:
-            return self._pretrain_forward(batch)
+            loss, predictions = self._pretrain_forward(batch)
+
+            if self.args.ewc:
+                # add EWC loss
+                ewc_loss = self.ewc_loss()
+                if self.args.ewc_lambda>0:
+                    loss += self.ewc_lambda * ewc_loss
+
+            return loss, predictions
         else:
-            return self._normal_forward(batch)
+            loss, predictions = self._normal_forward(batch)
+
+            if self.args.ewc:
+                # add EWC loss
+                ewc_loss = self.ewc_loss()
+                if self.args.ewc_lambda>0:
+                    loss += self.ewc_lambda * ewc_loss
+
+            return loss, predictions
         
         
     def get_encoder_loss(self, context_rnn_state):
@@ -125,18 +148,85 @@ class Seq2Seq(torch.nn.Module):
             encoder_loss += torch.std(groups_vals).item()
             
         return encoder_loss
-        
-        
-        
 
+    def ewc_loss(self):
+        '''Calculate EWC-loss.'''
+        if self.EWC_task_count>0:
+            losses = []
+            for n, p in self.named_parameters():
+                if p.requires_grad:
+                    # Retrieve stored mode (MAP estimate) and precision (Fisher Information matrix)
+                    n = n.replace('.', '__')
+                    mean = getattr(self, '{}_EWC_prev_task'.format(n))
+                    fisher = getattr(self, '{}_EWC_estimated_fisher'.format(n))
+                    # since "online EWC", apply decay-term to the running sum of the Fisher Information matrices
+                    fisher = self.ewc_gamma*fisher
+                    # Calculate EWC-loss
+                    losses.append((fisher * (p-mean)**2).sum())
+            # Sum EWC-loss from all parameters (and from all tasks, if "offline EWC")
+            return (1./2)*sum(losses)
+        else:
+            # EWC-loss is 0 if there are no stored mode and precision yet
+            return torch.tensor(0., device=self._device())
     
-            
-        
-    
-    
-    
-    
-    
-    
-    
-    
+    def estimate_fisher(self, data_loader):
+        '''After completing training on a task, estimate diagonal of Fisher Information matrix.'''
+
+        # Prepare <dict> to store estimated Fisher Information matrix
+        est_fisher_info = {}
+        for n, p in self.named_parameters():
+            if p.requires_grad:
+                n = n.replace('.', '__')
+                est_fisher_info[n] = p.detach().clone().zero_()
+
+        # Set model to evaluation mode
+        mode = self.training
+        self.eval()        
+
+        # Estimate the FI-matrix for [self.fisher_n] batches of size 1
+        for index,(x,y) in enumerate(data_loader):
+            # break from for-loop if max number of samples has been reached
+            if self.fisher_n is not None:
+                if index >= self.fisher_n:
+                    break
+            # run forward pass of model
+            x = x.to(self._device())
+            output = self(x)
+            # -use predicted label to calculate loglikelihood:
+            label = output.max(1)[1]
+            # calculate negative log-likelihood
+            negloglikelihood = F.nll_loss(F.log_softmax(output, dim=1), label)
+
+            # Calculate gradient of negative loglikelihood
+            self.zero_grad()
+            negloglikelihood.backward()
+
+            # Square gradients and keep running sum
+            for n, p in self.named_parameters():
+                if p.requires_grad:
+                    n = n.replace('.', '__')
+                    if p.grad is not None:
+                        est_fisher_info[n] += p.grad.detach() ** 2
+
+        # Normalize by sample size used for estimation
+        est_fisher_info = {n: p/index for n, p in est_fisher_info.items()}
+
+        # Store new values in the network
+        for n, p in self.named_parameters():
+            if p.requires_grad:
+                n = n.replace('.', '__')
+                # mode (MAP parameter estimate)
+                self.register_buffer('{}_EWC_prev_task'.format(n),
+                                        p.detach().clone())
+                # precision (approximated by diagonal Fisher Information matrix)
+                if self.EWC_task_count==1:
+                    existing_values = getattr(self, '{}_EWC_estimated_fisher'.format(n))
+                    est_fisher_info[n] += self.ewc_gamma * existing_values
+                self.register_buffer('{}_EWC_estimated_fisher'.format(n),
+                                        est_fisher_info[n])
+
+        # set task-count to 1 to indicate EWC-loss can be calculated
+        self.EWC_task_count = 1
+
+        # set model back to initial mode
+        self.train(mode=mode)
