3a20dad380f476105fbdd8d0f92cfa7e96a1011c
Ryan Othniel Kearns <ryankearns25@gmail.com>
2020-07-09 14:04:17 -0700

    more type error fixes

--- genienlp/models/general_seq2seq.py
+++ genienlp/models/general_seq2seq.py
@@ -185,7 +185,7 @@ class Seq2Seq(torch.nn.Module):
         self.eval()
 
         # Estimate the FI-matrix for batches of size 1
-        data_loader = self.fisher_data_loader(dataset, 1, cuda=self._is_on_cuda())
+        data_loader = self.fisher_data_loader(dataset, 1, self._is_on_cuda())
         for index, (x, y) in enumerate(data_loader):
 
             # run forward pass of model
@@ -231,7 +231,7 @@ class Seq2Seq(torch.nn.Module):
         # set model back to initial mode
         self.train(mode=mode)
 
-    def fisher_data_loader(dataset, batch, cuda=False, collate_fn=None, drop_last=False):
+    def fisher_data_loader(dataset, batch, cuda):
         '''Return <DataLoader>-object for the provided <DataSet>-object [dataset].'''
 
         # Create and return the <DataLoader>-object
