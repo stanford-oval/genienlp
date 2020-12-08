97817b1f5c396a5bac582520ec8272621ef5919f
Giovanni Campagna <gcampagn@cs.stanford.edu>
2020-12-07 22:49:42 -0800

    Remove a lot of unused code

--- genienlp/models/common.py
+++ genienlp/models/common.py
@@ -71,26 +71,6 @@ class MultiLSTMCell(nn.Module):
         return input, (h_1, c_1)
 
 
-def positional_encodings_like(x, t=None):
-    if t is None:
-        positions = torch.arange(0., x.size(1))
-        if x.is_cuda:
-            positions = positions.cuda(x.get_device())
-    else:
-        positions = t
-    encodings = torch.zeros(*x.size()[1:])
-    if x.is_cuda:
-        encodings = encodings.cuda(x.get_device())
-    for channel in range(x.size(-1)):
-        if channel % 2 == 0:
-            encodings[:, channel] = torch.sin(
-                positions / 10000 ** (channel / x.size(2)))
-        else:
-            encodings[:, channel] = torch.cos(
-                positions / 10000 ** ((channel - 1) / x.size(2)))
-    return Variable(encodings)
-
-
 # torch.matmul can't do (4, 3, 2) @ (4, 2) -> (4, 3)
 def matmul(x, y):
     if x.dim() == y.dim():
@@ -98,16 +78,6 @@ def matmul(x, y):
     if x.dim() == y.dim() - 1:
         return (x.unsqueeze(-2) @ y).squeeze(-2)
     return (x @ y.unsqueeze(-2)).squeeze(-2)
-
-
-def pad_to_match(x, y):
-    x_len, y_len = x.size(1), y.size(1)
-    if x_len == y_len:
-        return x, y
-    extra = x.new_ones((x.size(0), abs(y_len - x_len)))
-    if x_len < y_len:
-        return torch.cat((x, extra), 1), y
-    return x, torch.cat((y, extra), 1)
 
 
 class LayerNorm(nn.Module):
@@ -124,146 +94,6 @@ class LayerNorm(nn.Module):
         return self.gamma * (x - mean) / (std + self.eps) + self.beta
 
 
-class ResidualBlock(nn.Module):
-
-    def __init__(self, layer, d_model, dropout_ratio):
-        super().__init__()
-        self.layer = layer
-        self.dropout = nn.Dropout(dropout_ratio)
-        self.layernorm = LayerNorm(d_model)
-
-    def forward(self, *x, padding=None):
-        return self.layernorm(x[0] + self.dropout(self.layer(*x, padding=padding)))
-
-
-class Attention(nn.Module):
-
-    def __init__(self, d_key, dropout_ratio, causal):
-        super().__init__()
-        self.scale = math.sqrt(d_key)
-        self.dropout = nn.Dropout(dropout_ratio)
-        self.causal = causal
-
-    def forward(self, query, key, value, padding=None):
-        dot_products = matmul(query, key.transpose(1, 2))
-        if query.dim() == 3 and self.causal:
-            tri = key.new_ones((key.size(1), key.size(1))).triu(1) * INF
-            dot_products.sub_(tri.unsqueeze(0))
-        if padding is not None:
-            if dot_products.dim() == 3:
-                # dot_products is batch x query time x key time
-                # padding is batch x key time
-                # unsqueeze to batch x 1 x key time then broadcast
-                dot_products.masked_fill_(padding.unsqueeze(1).expand_as(dot_products), -INF)
-            else:
-                # dot_products is batch x key time and is directly compatible with padding
-                dot_products.masked_fill_(padding, -INF)
-        return matmul(self.dropout(F.softmax(dot_products / self.scale, dim=-1)), value)
-
-
-class MultiHead(nn.Module):
-
-    def __init__(self, d_key, d_value, n_heads, dropout_ratio, causal=False):
-        super().__init__()
-        self.attention = Attention(d_key, dropout_ratio, causal=causal)
-        self.wq = Linear(d_key, d_key, bias=False)
-        self.wk = Linear(d_key, d_key, bias=False)
-        self.wv = Linear(d_value, d_value, bias=False)
-        self.n_heads = n_heads
-
-    def forward(self, query, key, value, padding=None):
-        query, key, value = self.wq(query), self.wk(key), self.wv(value)
-        query, key, value = (
-            x.chunk(self.n_heads, -1) for x in (query, key, value))
-        return torch.cat([self.attention(q, k, v, padding=padding)
-                          for q, k, v in zip(query, key, value)], -1)
-
-
-class LinearReLU(nn.Module):
-
-    def __init__(self, d_model, d_hidden):
-        super().__init__()
-        self.feedforward = Feedforward(d_model, d_hidden, activation='relu')
-        self.linear = Linear(d_hidden, d_model)
-
-    def forward(self, x, padding=None):
-        return self.linear(self.feedforward(x))
-
-
-class TransformerEncoderLayer(nn.Module):
-
-    def __init__(self, dimension, n_heads, hidden, dropout):
-        super().__init__()
-        self.selfattn = ResidualBlock(
-            MultiHead(
-                dimension, dimension, n_heads, dropout),
-            dimension, dropout)
-        self.feedforward = ResidualBlock(
-            LinearReLU(dimension, hidden),
-            dimension, dropout)
-
-    def forward(self, x, padding=None):
-        return self.feedforward(self.selfattn(x, x, x, padding=padding))
-
-
-class TransformerEncoder(nn.Module):
-
-    def __init__(self, dimension, n_heads, hidden, num_layers, dropout):
-        super().__init__()
-        self.layers = nn.ModuleList(
-            [TransformerEncoderLayer(dimension, n_heads, hidden, dropout) for i in range(num_layers)])
-        self.dropout = nn.Dropout(dropout)
-
-    def forward(self, x, padding=None):
-        x = self.dropout(x)
-        encoding = [x]
-        for layer in self.layers:
-            x = layer(x, padding=padding)
-            encoding.append(x)
-        return encoding
-
-
-class TransformerDecoderLayer(nn.Module):
-
-    def __init__(self, dimension, n_heads, hidden, dropout, causal=True):
-        super().__init__()
-        self.selfattn = ResidualBlock(
-            MultiHead(dimension, dimension, n_heads,
-                      dropout, causal),
-            dimension, dropout)
-        self.attention = ResidualBlock(
-            MultiHead(dimension, dimension, n_heads,
-                      dropout),
-            dimension, dropout)
-        self.feedforward = ResidualBlock(
-            LinearReLU(dimension, hidden),
-            dimension, dropout)
-
-    def forward(self, x, encoding, selfattn_keys=None, context_padding=None, answer_padding=None):
-        if selfattn_keys is None:
-            selfattn_keys = x
-        x = self.selfattn(x, selfattn_keys, selfattn_keys, padding=answer_padding)
-        return self.feedforward(self.attention(x, encoding, encoding, padding=context_padding))
-
-
-class TransformerDecoder(nn.Module):
-
-    def __init__(self, dimension, n_heads, hidden, num_layers, dropout, causal=True):
-        super().__init__()
-        self.layers = nn.ModuleList(
-            [TransformerDecoderLayer(dimension, n_heads, hidden, dropout, causal=causal) for i in range(num_layers)])
-        self.dropout = nn.Dropout(dropout)
-        self.d_model = dimension
-
-    def forward(self, x, encoding, context_padding=None, positional_encodings=True, answer_padding=None):
-        if positional_encodings:
-            x = x + positional_encodings_like(x)
-        x = self.dropout(x)
-        for layer, enc in zip(self.layers, encoding[1:]):
-            x = layer(x, enc, context_padding=context_padding, answer_padding=answer_padding)
-        return x
-
-
 def mask(targets, out, squash=True, pad_idx=1):
     mask = (targets != pad_idx)
     out_mask = mask.unsqueeze(-1).expand_as(out).contiguous()
@@ -273,30 +103,6 @@ def mask(targets, out, squash=True, pad_idx=1):
         out_after = out * out_mask.float()
     targets_after = targets[mask]
     return out_after, targets_after
-
-
-class Highway(torch.nn.Module):
-    def __init__(self, d_in, activation='relu', n_layers=1):
-        super(Highway, self).__init__()
-        self.d_in = d_in
-        self._layers = torch.nn.ModuleList([Linear(d_in, 2 * d_in) for _ in range(n_layers)])
-        for layer in self._layers:
-            layer.bias[d_in:].fill_(1)
-        self.activation = getattr(F, activation)
-
-    def forward(self, inputs):
-        current_input = inputs
-        for layer in self._layers:
-            projected_input = layer(current_input)
-            linear_part = current_input
-            nonlinear_part = projected_input[:, :self.d_in] if projected_input.dim() == 2 \
-                else projected_input[:, :, :self.d_in]
-            nonlinear_part = self.activation(nonlinear_part)
-            gate = projected_input[:, self.d_in:(2 * self.d_in)] if projected_input.dim() == 2 \
-                else projected_input[:, :, self.d_in:(2 * self.d_in)]
-            gate = F.sigmoid(gate)
-            current_input = gate * linear_part + (1 - gate) * nonlinear_part
-        return current_input
 
 
 class LinearFeedforward(nn.Module):
@@ -309,33 +115,6 @@ class LinearFeedforward(nn.Module):
 
     def forward(self, x):
         return self.dropout(self.linear(self.feedforward(x)))
-
-
-class PackedLSTM(nn.Module):
-
-    def __init__(self, d_in, d_out, bidirectional=False, num_layers=1,
-                 dropout=0.0, batch_first=True):
-        """A wrapper class that packs input sequences and unpacks output sequences"""
-        super().__init__()
-        if bidirectional:
-            d_out = d_out // 2
-        self.rnn = nn.LSTM(d_in, d_out,
-                           num_layers=num_layers,
-                           dropout=dropout,
-                           bidirectional=bidirectional,
-                           batch_first=batch_first)
-        self.batch_first = batch_first
-
-    def forward(self, inputs, lengths, hidden=None):
-        lens, indices = torch.sort(lengths.clone().detach(), 0, True)
-        inputs = inputs[indices] if self.batch_first else inputs[:, indices]
-        outputs, (h, c) = self.rnn(pack(inputs, lens.tolist(),
-                                        batch_first=self.batch_first), hidden)
-        outputs = unpack(outputs, batch_first=self.batch_first)[0]
-        _, _indices = torch.sort(indices, 0)
-        outputs = outputs[_indices] if self.batch_first else outputs[:, _indices]
-        h, c = h[:, _indices, :], c[:, _indices, :]
-        return outputs, (h, c)
 
 
 class Linear(nn.Linear):
@@ -422,22 +201,6 @@ class CombinedEmbedding(nn.Module):
         return self._combine_embeddings(embedded)
 
 
-class SemanticFusionUnit(nn.Module):
-
-    def __init__(self, d, l):
-        super().__init__()
-        self.r_hat = Feedforward(d * l, d, 'tanh')
-        self.g = Feedforward(d * l, d, 'sigmoid')
-        self.dropout = nn.Dropout(0.2)
-
-    def forward(self, x):
-        c = self.dropout(torch.cat(x, -1))
-        r_hat = self.r_hat(c)
-        g = self.g(c)
-        o = g * r_hat + (1 - g) * x[0]
-        return o
-
-
 class LSTMDecoderAttention(nn.Module):
     def __init__(self, dim, dot=False):
         super().__init__()
@@ -478,50 +241,3 @@ class LSTMDecoderAttention(nn.Module):
         output = self.tanh(self.linear_out(combined_representation))
 
         return output, context_attention
-
-
-# The following was copied and adapted from Hugginface's Tokenizers library
-# Copyright 2018 The Google AI Language Team Authors and The HuggingFace Inc. team.
-# Copyright (c) 2018, NVIDIA CORPORATION.  All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-def mask_tokens(inputs: torch.Tensor, numericalizer, probability):
-    """ Prepare masked tokens inputs/labels for masked language modeling: 80% MASK, 10% random, 10% original. """
-
-    assert numericalizer.mask_id
-
-    labels = inputs.clone()
-    # We sample a few tokens in each sequence for masked-LM training
-    probability_matrix = torch.full(labels.shape, probability, device=inputs.device)
-    special_tokens_mask = [numericalizer.get_special_token_mask(token_ids) for token_ids in inputs.tolist()]
-    probability_matrix.masked_fill_(torch.tensor(special_tokens_mask, dtype=torch.bool, device=inputs.device),
-                                    value=0.0)
-    padding_mask = labels.eq(numericalizer.pad_id)
-    probability_matrix.masked_fill_(padding_mask, value=0.0)
-    masked_indices = torch.bernoulli(probability_matrix).bool()
-
-    # All tokens that are not masked become padding, so we don't compute loss on them
-    labels[~masked_indices] = numericalizer.pad_id
-
-    # 80% of the time, we replace masked input tokens with tokenizer.mask_token ([MASK])
-    indices_replaced = torch.bernoulli(torch.full(labels.shape, 0.8, device=inputs.device)).bool() & masked_indices
-    inputs[indices_replaced] = numericalizer.mask_id
-
-    # 10% of the time, we replace masked input tokens with random word
-    indices_random = torch.bernoulli(torch.full(labels.shape, 0.5, device=inputs.device)).bool() & masked_indices & ~indices_replaced
-    random_words = torch.randint(numericalizer.num_tokens, labels.shape, dtype=torch.long, device=inputs.device)
-    inputs[indices_random] = random_words[indices_random]
-
-    # The rest of the time (10% of the time) we keep the masked input tokens unchanged
-    return inputs, labels
