e680cbc2da1ea6d2c754b16463fb24216c8b032b
Jim Deng <jim.deng@gmail.com>
2021-01-15 01:12:34 +0000

    fix indents

--- dockerfiles/start.sh
+++ dockerfiles/start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -ex
 if [ -n "${S3_MODEL_DIR}" ]; then
-  mkdir model
   aws s3 sync --exclude '*/dataset/*' --exclude '*/cache/*' --exclude 'iteration_*.pth' --exclude '*_optim.pth' "${S3_MODEL_DIR}" ./model
   genienlp kfserver --path ./model $*
 else
