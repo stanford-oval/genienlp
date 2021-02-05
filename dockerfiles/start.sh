5f937192e39a92df3a9c9ba8955fcae42b15d683
Jim Deng <jim.deng@gmail.com>
2021-02-05 20:14:59 +0000

    one dockerfile

---
+++ dockerfiles/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -ex
+
+if [ -n "${S3_MODEL_DIR}" ]; then
+  aws s3 sync --exclude '*/dataset/*' --exclude '*/cache/*' --exclude 'iteration_*.pth' --exclude '*_optim.pth' "${S3_MODEL_DIR}" ./model
+  genienlp kfserver --path ./model $@
+else
+  genienlp $@
+fi
+
