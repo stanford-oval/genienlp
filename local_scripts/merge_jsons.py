7e390176d94064da244521c41686b8e5e81762d4
mehrad <mehrad@stanford.edu>
2021-02-19 12:46:28 -0800

    Add local scripts

---
+++ local_scripts/merge_jsons.py
@@ -0,0 +1,22 @@
+import ujson
+import os
+import sys
+
+data_folder = sys.argv[1]
+file_name = sys.argv[2]
+final_data = {}
+
+i = 0
+
+for file in os.listdir(data_folder):
+    if file.startswith(file_name):
+        full_path = os.path.join(data_folder, file)
+        with open(full_path, 'r') as fin:
+            data = ujson.load(fin)
+        print(f'processed file {i}')
+        i += 1
+        final_data.update(data)
+        
+with open(os.path.join(data_folder, f'{file_name}_final.json'), 'w') as fout:
+    ujson.dump(final_data, fout)
+    
