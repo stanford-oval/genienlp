122e9841bbf6301f26ac456315a0b04e9a0055ff
Giovanni Campagna <gcampagn@cs.stanford.edu>
2021-02-19 17:01:51 -0800

    Fix docker start script

--- dockerfiles/start.sh
+++ dockerfiles/start.sh
@@ -5,7 +5,7 @@ if [ -n "${S3_MODEL_DIR}" ]; then
   aws s3 sync --no-progress --exclude '*/dataset/*' --exclude '*/cache/*' --exclude 'iteration_*.pth' --exclude '*_optim.pth' "${S3_MODEL_DIR}" ./model
 fi
 if [ -n "${S3_DATABASE_DIR}" ]; then
-  aws s3 sync --no-progress "${S3_DATABASE_DIR}" --exclude '*' --include '*es_material*' --include "*${bootleg_model}/*" --include '*emb_data*' --include '*wiki_entity_data*' ./database
+  aws s3 sync --no-progress "${S3_DATABASE_DIR}" ./database --exclude '*' --include '*es_material*' --include "*${bootleg_model}/*" --include '*emb_data*' --include '*wiki_entity_data*'
 fi
 exec genienlp $@
 
