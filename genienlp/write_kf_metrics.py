f66d6d52cc259634a5833dfce6a9a8f00dcd150d
mehrad <mehrad@stanford.edu>
2021-03-14 13:37:05 -0700

    Add write_kf_metrics command

---
+++ genienlp/write_kf_metrics.py
@@ -0,0 +1,53 @@
+# Copyright 2021 The Board of Trustees of the Leland Stanford Junior University
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#  list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#  this list of conditions and the following disclaimer in the documentation
+#  and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#  contributors may be used to endorse or promote products derived from
+#  this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+import json
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def parse_argv(parser):
+    parser.add_argument('--eval_file', type=str, required=True, help='path to eval json file')
+
+
+def main(args):
+    with open(args.eval_file, 'r') as fin:
+        metrics = json.load(fin)
+    
+    # kf dashboard is hardcoded to only show two metrics
+    # we choose the first two from task metrics that are important
+    items = list(metrics.items())[:2]
+    
+    extracted_metrics = [
+        {'name': key, 'numberValue': val / 100, 'format': "PERCENTAGE"} for key, val in items
+    ]
+    
+    metrics = {'metrics': extracted_metrics}
+    with open('/tmp/mlpipeline-metrics.json', 'w') as fout:
+        json.dump(metrics, fout)
