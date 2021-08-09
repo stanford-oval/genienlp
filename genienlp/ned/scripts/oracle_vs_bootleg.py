3c7211ec833fb50e6b46c4e6b6e921c440d65857
mehrad <mehrad@stanford.edu>
2021-08-09 13:30:15 -0700

    Update ned scripts

--- genienlp/ned/scripts/oracle_vs_bootleg.py
+++ genienlp/ned/scripts/oracle_vs_bootleg.py
@@ -4,7 +4,6 @@ from collections import Counter, defaultdict
 import jsonlines
 
 from genienlp.ned import BatchBootlegEntityDisambiguator
-from genienlp.ned.ned_utils import reverse_bisect_left
 
 parser = argparse.ArgumentParser()
 
@@ -19,13 +18,21 @@ args = parser.parse_args()
 if __name__ == '__main__':
 
     args.bootleg_model = 'bootleg_uncased_mini'
-    args.ned_normalize_types = 'yes'
+
+    # args.ned_normalize_types = 'yes'
+    # args.bootleg_prob_threshold = 0.3
+    # args.max_types_per_qid = 1
+    # args.max_qids_per_entity = 1
+
+    args.ned_normalize_types = 'force'
+    args.bootleg_prob_threshold = 0.01
+    args.max_types_per_qid = 4
+    args.max_qids_per_entity = 3
+
     args.bootleg_output_dir = 'results_temp'
     args.embeddings = '.embeddings'
     args.almond_type_mapping_path = None
-    args.bootleg_prob_threshold = 0.3
-    args.max_types_per_qid = 1
-    args.max_qids_per_entity = 1
+
     args.max_features_size = args.max_types_per_qid * args.max_qids_per_entity
 
     bootleg = BatchBootlegEntityDisambiguator(args)
@@ -36,6 +43,8 @@ if __name__ == '__main__':
     not_detected = Counter()
     no_TT_type = Counter()
     not_correct = Counter()
+
+    fout = open('comparison.txt', 'w')
 
     for oracle_line, bootleg_line in zip(oracle_lines, bootleg_lines):
 
@@ -52,29 +61,32 @@ if __name__ == '__main__':
         for alias, all_qids, all_probs, span in zip(
             bootleg_line['aliases'], bootleg_line['cands'], bootleg_line['cand_probs'], bootleg_line['spans']
         ):
-            # filter qids with confidence lower than a threshold
-            idx = reverse_bisect_left(all_probs, args.bootleg_prob_threshold)
-            all_qids = all_qids[:idx]
-            all_probs = all_probs[:idx]
-
             type_ids, type_probs, qids = bootleg.collect_features_per_alias(alias, all_probs, all_qids)
 
             type_vocabs = [bootleg.typeqid_to_type_vocab[bootleg.id2typeqid[id_]] for id_ in type_ids]
 
             entity2types_bootleg[alias] = type_vocabs
 
-        for k, v in entity2types_oracle.items():
-            if k not in entity2types_bootleg:
-                if 'the ' + k not in entity2types_bootleg:
-                    not_detected[k] += 1
-                else:
-                    k = 'the ' + k
-            elif len(entity2types_bootleg[k]) == 0:
-                no_TT_type[k] += 1
-            elif v not in entity2types_bootleg[k]:
-                not_correct[k] += 1
-                # print(f'correct TT type: {entity2types_oracle[k]}')
-                # print(f'bootleg TT type: {entity2types_bootleg[k]}')
+        for k_oracle, v_oracle in entity2types_oracle.items():
+            detected = False
+            for k_boot in entity2types_bootleg.keys():
+                if k_oracle in k_boot or k_boot in k_oracle:
+                    detected = True
+                    break
+
+            if detected:
+                if len(entity2types_bootleg[k_boot]) == 0:
+                    no_TT_type[k_oracle] += 1
+                elif v_oracle not in entity2types_bootleg[k_boot]:
+                    not_correct[k_oracle] += 1
+                    fout.write(
+                        f'TT type for {k_oracle}: {entity2types_oracle[k_oracle]} ; {k_boot}: {entity2types_bootleg[k_boot]}'
+                        + '\n'
+                    )
+            else:
+                not_detected[k_oracle] += 1
+
+    fout.close()
 
     print(f'not_detected: {not_detected}')
     print(f'no TT type detected: {no_TT_type}')
