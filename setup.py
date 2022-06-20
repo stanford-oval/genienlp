b35ede4aff9ee0b1b81a086d363136607f87cd82
mehrad <mehrad@stanford.edu>
2022-06-20 10:52:57 -0700

    setup: update dialogues

--- setup.py
+++ setup.py
@@ -72,6 +72,6 @@ setuptools.setup(
         'dill~=0.3',
         'xgboost~=1.3',
         # for e2e dialogues:
-        'dialogues @ git+https://github.com/Mehrad0711/dialogues@525ebb209594360183d4fc30c7d9d03d24ed68b9',
+        'dialogues @ git+https://github.com/Mehrad0711/dialogues@77abfa00e79f49b146a054d288ad96c82ef92298',
     ],
 )
