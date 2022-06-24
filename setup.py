ae9261ed7f15e6e9fa41b7a1e5b57ee711cdc209
mehrad <mehrad@stanford.edu>
2022-06-24 10:08:16 -0700

    setup: switch back dialogue to main

--- setup.py
+++ setup.py
@@ -72,6 +72,6 @@ setuptools.setup(
         'dill~=0.3',
         'xgboost~=1.3',
         # for e2e dialogues:
-        'dialogues @ git+https://github.com/Magnetic2014/dialogues@tianhao',
+        'dialogues @ git+https://github.com/Mehrad0711/dialogues@main',
     ],
 )
