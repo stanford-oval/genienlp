d8dfe3bae6b33d453dd5289d2ed476886479e0a3
mehrad <mehrad@stanford.edu>
2022-08-09 11:50:15 -0700

    base: bring back model hack (to be removed later)

--- genienlp/models/base.py
+++ genienlp/models/base.py
@@ -37,7 +37,7 @@ import dialogues
 import torch
 import ujson
 from dateparser.languages import default_loader
-from transformers import AutoConfig, MarianTokenizer, PreTrainedModel
+from transformers import AutoConfig, BartForConditionalGeneration, MarianTokenizer, PreTrainedModel
 
 from ..data_utils.example import NumericalizedExamples, SequentialField
 from ..data_utils.numericalizer import TransformerNumericalizer
@@ -67,6 +67,19 @@ class GenieModel(PreTrainedModel):
         logger.info(f'Loading the model from {full_checkpoint_path}')
         model = cls(args=args, tasks=tasks, vocab_sets=vocab_sets, save_directory=save_directory, *model_args, **kwargs)
         save_dict = torch.load(full_checkpoint_path, map_location=device)
+
+        # HACK
+        # `transformers` version 4.1 changed the name of language modeling head of BartForConditionalGeneration
+        # (and therefore its subclass MBartForConditionalGeneration) to lm_head to make it similar to other models
+        # like T5. The following will make this change so that genienlp models trained with `transformers`==4.0 can be properly loaded
+
+        # TODO: remove this once we make sure the new paraphraser runs fine
+        if (
+            'model.lm_head.weight' not in save_dict['model_state_dict']
+            and 'model.model.shared.weight' in save_dict['model_state_dict']
+            and isinstance(model.model, BartForConditionalGeneration)
+        ):
+            save_dict['model_state_dict']['model.lm_head.weight'] = save_dict['model_state_dict']['model.model.shared.weight']
 
         model.load_state_dict(save_dict['model_state_dict'], strict=True)
 
