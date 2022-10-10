9b01123155f00d939ca8eb45214eb916b1718a68
mehrad <mehrad@stanford.edu>
2022-10-10 16:31:55 -0700

    Fix requote_program for numbers

--- genienlp/util.py
+++ genienlp/util.py
@@ -55,6 +55,7 @@ logger = logging.getLogger(__name__)
 
 ENTITY_MATCH_REGEX = re.compile('^([A-Z].*)_[0-9]+$')
 QUOTED_MATCH_REGEX = re.compile(' " (.*?) " ')
+NUMBER_MATCH_REGEX = re.compile('^([0-9]+\.?[0-9]?)$')
 
 
 def find_span_type(program, begin_index, end_index):
@@ -107,8 +108,11 @@ def requote_program(program):
 
         elif not in_string:
             entity_match = ENTITY_MATCH_REGEX.match(token)
+            number_match = NUMBER_MATCH_REGEX.match(token)
             if entity_match is not None:
                 requoted.append(entity_match[1])
+            elif number_match is not None:
+                requoted.append('NUMBER')
             elif token != 'location:':
                 requoted.append(token)
 
